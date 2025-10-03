d64c426b645d2b56a10c8a14575fc1f9428db5c8
andmigle-lang <and.migle@gmail.com>
2025-10-03 11:03:24 +0300

    Update Generuoti_failai.h

--- Generuoti_failai.h
+++ Generuoti_failai.h
@@ -11,8 +11,7 @@
 #include <ctime>
 #include <chrono>
 #include "palyginimas.h"
-#include <iterator>     // naudoja istreambuf_iterator
 
 void failu_generavimas(int k);
-void studentu_rusiavimas(vector <Studentas>& Grupe, int k, double& diff_rusiavimas1, double& diff_irasu_dalijimo1, double& diff_irasymas_i_vargsiuku_faila1, double& diff_irasymas_i_kietiaku_faila1);
+void studentu_rusiavimas(vector <Studentas>& Grupe, int k, double& diff_rusiavimas1, double& diff_irasu_dalijimo1, double& diff_irasymas_i_vargsiuku_faila1, double& diff_irasymas_i_kietiaku_faila1, int i);
 void skaitymas_is_genruoto_failo(vector<Studentas>& Grupe, const string& failo_vardas, double& diff_skaitymas1);

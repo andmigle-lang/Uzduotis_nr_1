9edc39a5def41be7647b279eb77039ce2ce41dde
andmigle-lang <and.migle@gmail.com>
2025-10-30 14:39:30 +0200

    Pridėtos reikalingos bibliotekos ir #using sakiniai

--- Generuoti_failai.h
+++ Generuoti_failai.h
@@ -13,7 +13,32 @@
 #include "palyginimas.h"
 #include <random>
 
+#include <vector>
+#include <list>
+#include <algorithm>    // for sort
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::setw;
+using std::stringstream;
+using std::left;
+using std::right;
+using std::ofstream;
+using std::ifstream;
+using std::accumulate;
+using std::to_string;
+using std::fixed;
+using std::setprecision;
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+using std::random_device;
+using std::mt19937;
+using std::uniform_int_distribution;
+
 void failu_generavimas(int k, int pazymiu_sk);
+
 template <typename T> void pagalbine_funkcija(vector <T>& Grupe, int pasirinkimas) {
     if (pasirinkimas == 1) {
         sort(Grupe.begin(), Grupe.end(), palyginimas_vardas);
@@ -106,4 +131,71 @@ template <typename T> void studentu_rusiavimas(T& Grupe, int k,
     cout << "Surusiuota studentu:" << endl;
     cout << "Vargsai (< 5.0): " << vargsiukai.size() << " studentai" << endl;
     cout << "Kietiakai (>= 5.0): " << kietiakai.size() << " studentai" << endl;
+}
+
+template <typename T> void skaitymas_is_generuoto_failo(T& Grupe, const string& failo_vardas, double& diff_skaitymas1) {
+    auto start_skaitymas = high_resolution_clock::now();
+    ifstream F(failo_vardas);
+    if (!F) {
+        cout << "Klaida: Nepavyko atidaryti failo " << failo_vardas << endl;
+        exit(0);
+    }
+    string pavadinimai;
+    getline(F, pavadinimai);
+    Studentas Pirmas;
+    int skait = 0;
+    while (F.peek() != EOF) {
+        F >> Pirmas.var >> Pirmas.pav;
+        if (F.fail() || Pirmas.var.size() == 0 || Pirmas.pav.size() == 0) {
+            F.clear();
+            break;
+        }
+        skait++;
+        float mediana;
+        int sum = 0;
+        Pirmas.paz.clear();
+        while (F.peek() != '\n' && F.peek() != EOF) {
+            int pazymys;
+            string pazymys_pr;
+            F >> pazymys_pr;
+            if (isNumber(pazymys_pr)) {
+                if (stoi(pazymys_pr) >= 1 && stoi(pazymys_pr) <= 10) {
+                    pazymys = stoi(pazymys_pr);
+                    Pirmas.paz.push_back(pazymys);
+                }
+                else {
+                    cout << "Studento nr. " << skait << " pazymiuose buvo klaida (ne sveikasis skaicius nuo 1 iki 10): " << pazymys_pr << ". Klaida pasalinta is skaiciavimu" << endl;
+                }
+            }
+            else {
+                cout << "Studento nr. " << skait << " pazymiuose buvo klaida (ne sveikasis skaicius nuo 1 iki 10): " << pazymys_pr << ". Klaida pasalinta is skaiciavimu" << endl;
+            }
+        }
+        if (Pirmas.paz.size() != 0) {
+            Pirmas.egz = Pirmas.paz.back();
+            Pirmas.paz.pop_back();
+        }
+        else {
+            Pirmas.egz = 0;
+        }
+        sum = accumulate(Pirmas.paz.begin(), Pirmas.paz.end(), 0);
+        int n;
+        n = Pirmas.paz.size();
+        if (Pirmas.paz.size() == 0) {
+            Pirmas.gal = Pirmas.egz * 0.6;
+            Pirmas.med = Pirmas.egz * 0.6;
+        }
+        else {
+            Pirmas.gal = double(sum) / double(n) * 0.4 + Pirmas.egz * 0.6;
+            mediana = Rask_mediana(Pirmas.paz);
+            Pirmas.med = mediana * 0.4 + Pirmas.egz * 0.6;
+        }
+        Grupe.push_back(Pirmas);
+        Pirmas.paz.clear();
+    }
+    F.close();
+    duration<double> diff_skaitymas = high_resolution_clock::now() - start_skaitymas;
+    cout << "\nFailo is " << Grupe.size() << " irasu nuskaitymo laikas: " << diff_skaitymas.count() << " s" << endl;
+    diff_skaitymas1 = diff_skaitymas.count();
+    cout << "Duomenys nuskaityti is failo. Rastas studentu skaicius: " << Grupe.size() << endl;
 }

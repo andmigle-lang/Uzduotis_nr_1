402c41367561f333d8bf375bdf6eefd474aab80e
andmigle-lang <and.migle@gmail.com>
2025-11-04 21:09:57 +0200

    Pridėta palyginimo funkcija, naudojama 2 strategijoje

--- palyginimas.h
+++ palyginimas.h
@@ -4,3 +4,5 @@
 bool palyginimas_vardas(const Studentas& pirm, const Studentas& antr);
 bool palyginimas_pavarde(const Studentas& pirm, const Studentas& antr);
 bool palyginimas_galutinis(const Studentas& pirm, const Studentas& antr);
+
+bool palyginimas_2_strategija(const Studentas& pirm, const Studentas& antr);
